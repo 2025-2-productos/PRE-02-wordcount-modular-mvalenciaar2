7f5095396d60d111c06a5df54cbf9ebbcdb580ec
Maria <mvalenciaar@unal.edu.co>
2025-11-22 07:59:19 -0500

    Se crea la función write_count_words.py

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,27 +1,29 @@
 # obtain a list of files in the input directory
 import os
 
-# def read_all_lines():
-#     all_lines = []
-#     input_files_list = os.listdir("data/input/")
+from .write_count_words import write_count_words
 
-#     for filename in input_files_list:
-#         with open(filename, "r", encoding="utf-8") as f:
-#             lines = f.readlines()
-#             all_lines.extend(lines)
-#     return all_lines
+
+def read_all_lines():
+    all_lines = []
+    input_files_list = os.listdir("data/input/")
+
+    for filename in input_files_list:
+        with open(filename, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            all_lines.extend(lines)
+    return all_lines
 
 
 def main():
-    ###
-    # all_lines = read_all_lines()
+
+    ##read all lines
+    # all_lines = []
     input_files_list = os.listdir("data/input/")
 
-    ### read all lines
     ### preprocess lines
     ### split in words
     ### count words
-    ### write count words
 
     # count the frequency of the words in the files in the input directory
     counter = {}
@@ -38,16 +40,17 @@ def main():
     write_count_words(counter)
 
 
-def write_count_words(counter):
-    if not os.path.exists("data/output"):
-        os.makedirs("data/output")
-
-    # save the results using tsv format
-    with open("data/output/results.tsv", "w", encoding="utf-8") as f:
-        for key, value in counter.items():
-            # write the key and value to the file
-            f.write(f"{key}\t{value}\n")
-
-
 if __name__ == "__main__":
     main()
+
+
+# create the directory output/ if it doesn't exist
+# def write_count_words():
+#     if not os.path.exists("data/output"):
+#         os.makedirs("data/output")
+
+#     # save the results using tsv format
+#     with open("data/output/results.tsv", "w", encoding="utf-8") as f:
+#         for key, value in counter.items():
+#             # write the key and value to the file
+#             f.write(f"{key}\t{value}\n")
